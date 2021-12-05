e192b746d1476fba407c195b2a2aebf6ed2b5f5a
elavaux0 <elavaux0@gmail.com>
2021-12-05 23:02:46 +0100

    class home & liste

---
+++ lib/pages/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Discovery',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Discovery'),
+        ),
+        body: Center(
+          child: OnPressed: ('Hello World'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/liste.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class _liste extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Discovery',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Discovery'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
