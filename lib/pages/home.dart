9d93f16d399774f483886bbd5cc607d8d398fc68
elavaux0 <elavaux0@gmail.com>
2021-12-06 22:08:38 +0100

    page accueil front

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,16 +12,41 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
+              new Image.asset(
+                'assets/LogoDiscovery.png',
+                width: 400.0,
+                height: 400.0,
+                fit: BoxFit.cover,
+              ),
               Container(
-                color: Colors.blue,
-                height: 140,
-                decoration: BoxDecoration(image: DecorationImage(image: AssetImage())),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () => {},
+                          child: Column(
+                            children: [
+                              Icon(Icons.login),
+                              Text("Connexion")
+                            ],
+                          ),
+                        ),
+                    TextButton(
+                    onPressed: () => {},
+                      child: Column(
+                        children: [
+                          Icon(Icons.logout),
+                          Text("Connexion")
+                        ],
+                      ),
+                    )
+                  ]
+                )
               )
-            ],
-          )
-
+          ]
         ),
       ),
+    )
     );
   }
 }
