beb2d96a87044dac0ad5f70a7e749f08fe1d7b5c
Le Page Brewen <brewenlepage@gmail.com>
2021-12-12 23:12:07 +0100

    Brewen mis a niveau

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 // @dart=2.9
-import 'package:appflutter/pages/authen.dart';
+//import 'package:appflutter/pages/authen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 import 'package:appflutter/pages/map.dart';
 //import './pages/profil.dart';
+import './pages/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,7 +22,7 @@ class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SplashScreen(
-        navigateAfterSeconds: map(),
+        navigateAfterSeconds: Home(),
         seconds: 5,
         title: const Text(
           'Discovery',
